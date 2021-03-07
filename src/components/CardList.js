f58caadf10f42e5326942d9e3ad77b1162899236
Mauricio G <mauriciogarzonb@gmail.com>
2021-03-07 18:31:29 -0500

    Error management

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -2,9 +2,6 @@ import React from 'react';
 import Card from './Card'
 
 const CardList = ({ robots }) => {
-    if (true) {
-        
-    }
     return (
         <div>
             {robots.map((user, i) => {
