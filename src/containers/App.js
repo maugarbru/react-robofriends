fec570a167bf0da4dd37ee5d914588c3da974adf
Mauricio G <mauriciogarzonb@gmail.com>
2021-03-13 17:58:54 -0500

    Redux

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,27 +3,29 @@ import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox'
 import Scroll from '../components/Scroll'
 import ErrorBoundry from '../components/ErrorBoundry'
-// import { robots } from './robots'
+import { setSearchField, requestRobots } from '../actions.js'
+import { connect } from 'react-redux'
 import './App.css'
 
+const mapStateToProps = state => {
+    return {
+        searchField: state.searchRobots.searchField,
+        robots: state.requestRobots.robots,
+        isPending: state.requestRobots.isPending,
+        error: state.requestRobots.error
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
+        onRequestRobots: () => dispatch(requestRobots())
+    }
+}
+
 function App() {
-
-    // constructor() {
-    //     super()
-    //     this.state = {
-    //         robots: [],
-    //         searchField: ''
-    //     }
-    // }
-
     const [robots, setRobots] = useState([])
     const [searchField, setSearchField] = useState('')
-
-    // componentDidMount() {
-    //     fetch('https://jsonplaceholder.typicode.com/users')
-    //         .then(response => response.json())
-    //         .then(users => this.setState({ robots: users }))
-    // }
 
     useEffect(() => {
         fetch('https://jsonplaceholder.typicode.com/users')
@@ -60,4 +62,4 @@ function App() {
 
 }
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App)
--- src/index.js
+++ src/index.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import reportWebVitals from './reportWebVitals';
-
-import './index.css';
+import { Provider } from 'react-redux';
+import thunkMiddleware from 'redux-thunk';
+import { applyMiddleware, createStore, combineReducers } from 'redux';
+import { searchRobots, requestRobots } from './reducers';
+import { createLogger } from 'redux-logger';
 import App from './containers/App';
+import './index.css';
 import 'tachyons'
 
+const logger = createLogger()
+
+const rootReducer = combineReducers({ searchRobots, requestRobots })
+const store = createStore(rootReducer, applyMiddleware(thunkMiddleware, logger))
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
